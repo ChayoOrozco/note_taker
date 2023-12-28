24c382cd558462c98c1b7132e35cb8e3e3c43d92
Chayo Orozco <84361768+ChayoOrozco@users.noreply.github.com>
2023-12-28 16:44:38 -0600

    Version actualizada

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-#Cheating and taking notes
+from datetime import datetime
+
 def show_menu():
-    print("1.Add new note")
-    print("2. Print notes")
-    print("3.Salir")
+    print("1. Agregar Nota")
+    print("2. Crear nuevo archivo")
+    print("3. Salir")
+
 def add_note(notes):
-    note = input("Write here")
+    note = input("Escribe aqui " + "\n" )
     notes.append(note)
-def print_note(notes):
-    for note in notes:
-        print(note)
+    print("Guardado.")
+
+def save_notes_to_file(notes):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"notes_{timestamp}.txt"
+    with open(filename, "w") as file:
+        for note in notes:
+            file.write(note + "\n")
+    print(f"Notes saved to {filename}")
+
 def main():
     notes = []
     while True:
         show_menu()
-        option = input ("Choose")
+        option = input("Choose an option: ")
         if option == "1":
             add_note(notes)
-        elif option == "2":
-            print_note(notes)
         elif option == "3":
+            save_notes_to_file(notes)
+        elif option == "4":
+            print("Exiting program.")
             break
         else:
-            print("Nope, try again")
+            print("Invalid option, please try again.")
+
 if __name__ == "__main__":
     main()
